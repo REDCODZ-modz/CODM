b6c972a07646be92eee44ce51f6883e01d0e4b17
REDCODZ-modz <r3d.c0d3.m0dz.@gmail.com>
2025-05-24 23:52:03 +0700

    main.lua

--- main.lua
+++ main.lua
@@ -13,7 +13,6 @@ v[1].freeze = true
 gg.setValues(v)
 end
 
-redline_status = false
 
 RedCode("libunity.so", 0x84a32a0, "h20008052C0035FD6", 32); 
 RedCode("libunity.so", 0x84a3954, "h20008052C0035FD6", 32); 
@@ -34,6 +33,8 @@ RedCode("libunity.so", 0x543f828, "hC0035FD6C0035FD6", 32);
 
 RED = 1
 
+redline_status = false
+
 bypass_status = false
 function toggleBypass()
     if bypass_status then
@@ -48,14 +49,15 @@ function toggleBypass()
 end
 function main()
     local bypassText = bypass_status and "    ⃢🟢🔸  𝗕𝗬𝗣𝗔𝗦𝗦 𝙇𝙊𝙂𝙊" or "🔴⃢    🔸  𝗕𝗬𝗣𝗔𝗦𝗦 𝙇𝙊𝙂𝙊"
-    local redlineText = redline_status and "    ⃢🟢🔸 𝗥𝗘𝗗 𝗘𝗦𝗣 𝗟𝗜𝗡𝗘 {INGAME}" or "🔴⃢    🔸 𝗥𝗘𝗗 𝗘𝗦𝗣 𝗟𝗜𝗡𝗘 {INGAME}"
+    local redlineText = redline_status and "    ⃢🟢🔸 𝗥𝗘𝗗 𝗟𝗜𝗡𝗘 {INGAME}" or "🔴⃢    🔸 𝗥𝗘𝗗 𝗟𝗜𝗡𝗘 {INGAME}"
     RED = gg.choice({
         bypassText,
+        redlineText,
         "ᯓ➤ 𝗖𝗟𝗘𝗔𝗥 𝗟𝗢𝗚𝗦",
         "ᯓ➤ 𝙎𝙆𝙄𝙋 𝙏𝙐𝙏𝙊𝙍𝙄𝘼𝙇",
-        redlineText,
         "ᯓ➤ 𝗔𝗜𝗠𝗕𝗢𝗧 𝗠𝗘𝗡𝗨",
         "ᯓ➤ 𝗛𝗔𝗖𝗞𝗦 𝗠𝗘𝗡𝗨",
+        "ᯓ➤ 𝙃𝘼𝘾𝙆𝙎 𝙎𝙆𝙄𝙉 𝘾𝙃𝘼𝙍",
         "ᯓ➤ 𝗘𝗫𝗜𝗧 𝗠𝗘𝗡𝗨",
     }, nil, [[
 
@@ -69,12 +71,13 @@ function main()
     
     if RED == nil then return end
     if RED == 1 then toggleBypass() end
-    if RED == 2 then logs() end
-    if RED == 3 then skip() end
-    if RED == 4 then redline() end
+    if RED == 2 then redline() end
+    if RED == 3 then logs() end
+    if RED == 4 then skip() end
     if RED == 5 then aim() end
     if RED == 6 then cheat() end
-    if RED == 7 then os.exit() end
+    if RED == 7 then skin() end
+    if RED == 8 then os.exit() end
 end
     RED = -1
 
@@ -154,10 +157,9 @@ function redline()
         results = gg.getResults(99999)
         gg.editAll("h 00 00 80 40 00 00 80 3F 00 00 80 3F 00 00 00 00 00 00 00 00 00 00 40 40", gg.TYPE_BYTE)
         gg.toast("RED LINE DEACTIVATED")
-        gg.alert(" RED LINE DINONAKTIFKAN.\n\nAktifkan kembali saat didalam macth jika diperlukan.",
+        gg.alert("╔════════════════════╗\n║   RED LINE DINONAKTIFKAN   ║\n╚════════════════════╝\n\n• Harap menonaktifkan fitur ini setelah match berakhir untuk stabilitas maksimal.",
         
-        
-        "OK"," BATAL "
+        "OK"
         )
         redline_status = false
     else
@@ -167,10 +169,10 @@ function redline()
         results = gg.getResults(99999)
         gg.editAll("h 00 00 48 42 00 00 80 3F 00 00 96 43 00 00 00 00 00 00 00 00 00 00 40 40", gg.TYPE_BYTE)
         gg.toast("RED LINE ACTIVATED")
-        gg.alert("RED LINE DIAKTIFKAN.\n\nNONAKTIFKAN kembali jika macth telah selesai.",
+        gg.alert("╔══════════════════════════╗\n║    RED LINE TELAH AKTIF    ║\n╚══════════════════════════╝\n\n• Gunakan saat pertandingan.\n• Nonaktifkan setelah match untuk stabilitas.",
        
         
-          "OK","BATAL"
+          "OK"
         )
         redline_status = true
     end
@@ -180,7 +182,7 @@ end
 
 function aim()
     local menu = gg.multiChoice({
-        "ᯓ➤ 𝗔𝗜𝗠𝗕𝗢𝗧 𝗔𝗟𝗟 𝗚𝗨𝗡𝗦 {max 200}",  
+        "ᯓ➤ 𝗔𝗜𝗠𝗕𝗢𝗧 𝗔𝗟𝗟 𝗚𝗨𝗡𝗦",  
         "ᯓ➤ 𝗔𝗜𝗠𝗕𝗢𝗧 𝗟𝗢𝗖𝗞",  
     })
 
@@ -195,20 +197,34 @@ function aim()
 end
 
 function D1()
-AIMBOT1 = 0x98887fc
-AIMBOT2 = 0x83d45a0
-setValues(il2cpp + AIMBOT1, 32, "h4000001CC0035FD6")
-setValues(il2cpp + AIMBOT1 + 0x4, 32, "hC0035FD600007A44")
-setValues(il2cpp + AIMBOT2, 32, "h4000001CC0035FD6")
-setValues(il2cpp + AIMBOT2 + 0x4, 32, "hC0035FD600007A44")
-AIMBOT =gg.prompt({"AIMBOT ADJUSTABLE "},nil,{"number"})
-if AIMBOT and AIMBOT [1] then
-setValues(il2cpp + AIMBOT1 + 0x8, 16, AIMBOT[1])
-setValues(il2cpp + AIMBOT2 + 0x8, 16, AIMBOT[1])
-gg.toast("AIM ALL GUNS ACTIVATED")
+
+  local AIMBOT1 = 0x98887fc
+  local AIMBOT2 = 0x83d45a0
+
+  setValues(il2cpp + AIMBOT1, 32, "h4000001CC0035FD6")
+  setValues(il2cpp + AIMBOT1 + 0x4, 32, "hC0035FD600007A44")
+  setValues(il2cpp + AIMBOT2, 32, "h4000001CC0035FD6")
+  setValues(il2cpp + AIMBOT2 + 0x4, 32, "hC0035FD600007A44")
+
+  local AIMBOT = gg.prompt(
+    {"Aimbot Adjust Settings Max 200"},
+    {[1] = 200},
+    {[1] = "number"}
+  )
+
+  if AIMBOT and AIMBOT[1] then
+    if tonumber(AIMBOT[1]) > 200 then
+      gg.alert("Fatal Error: [0xC0000005]\nAccess Violation at address 0x0000001A.\nPlease restart the script system.")
+      os.exit()
+    end
+    setValues(il2cpp + AIMBOT1 + 0x8, 16, AIMBOT[1])
+    setValues(il2cpp + AIMBOT2 + 0x8, 16, AIMBOT[1])
+    gg.toast("AIM ALL GUNS ACTIVATED " .. AIMBOT[1])
+  else
+    gg.toast("Aimbot dibatalkan")
+  end
 end
 
-end
 function D2()
 gg.setRanges(gg.REGION_ANONYMOUS)
 gg.searchNumber("7.00649232e-45F;0.69999998808F;0.69999998808F;0.69999998808F;0.69999998808F;7.00649232e-45F;0.5F;0.5F;0.5F;0.60000002384F;0.60000002384F", gg.TYPE_FLOAT)
@@ -224,7 +240,7 @@ RED = gg.multiChoice({
 "𝗡𝗢𝗥𝗠𝗔𝗟 𝗪𝗔𝗟𝗟 𝗛𝗔𝗖𝗞 {SAFE LOBBY}", 
 "𝗥𝗘𝗗 𝗪𝗔𝗟𝗟 𝗛𝗔𝗖𝗞 {SAFE LOBBY}",
 "𝗟𝗘𝗦𝗦 𝗥𝗘𝗖𝗢𝗜𝗟 {SAFE LOBBY}",  
-"𝙀𝙎𝙋 𝙉𝘼𝙈𝙀 𝘽𝙍 {SAFE LOBBY}",
+"𝙉𝘼𝙈𝙀 𝙏𝘼𝙂 𝘽𝙍 {SAFE LOBBY}",
 "𝗡𝗢 𝗦𝗢𝗠𝗞𝗘 {SAFE INGAME}"
 })
 if RED == nil then main()else
@@ -236,8 +252,6 @@ if RED[5] == true then h5()end
 end
 RED = -1
 end
-
-
 
 function h1()
 RedCode("libunity.so", 0x6622bd0, "h1F 20 03 D5 E0 03 13 AA", 4);
@@ -270,7 +284,7 @@ function h4()
 RedCode("libunity.so", 0x5771040, "h20 00 80 D2 C0 03 5F D6", 32);
 RedCode("libunity.so", 0x57710DC, "h20 00 80 D2 C0 03 5F D6", 32);
 RedCode("libunity.so", 0x5844444, "h20 00 80 D2 C0 03 5F D6", 32);
-gg.toast(" ESP NAME BR ACTIVATED")
+gg.toast(" NAME TAG BR ACTIVATED")
 end
 
 
@@ -294,6 +308,148 @@ var = gg.getResults(9999)
 gg.editAll("0", gg.TYPE_DWORD)
 gg.clearResults()
 gg.toast("NO SMOKE ACTIVATED")
+end
+
+function skin()
+  local b = gg.multiChoice({
+    "➢ TEMPLAR MYTHIC",
+    "➢ SPECTRE MYTHIC",
+    "➢ NYX LEGEND",
+    "➢ DAME LEGEND",
+    "➢ DARK FIONA",
+    "➢ REAPER ASHURA",
+    "➢ HOLY FATHER",
+  }, nil, "CHARACTER SKINS")
+
+  if b == nil then return end
+  if b[1] then c19() end
+  if b[2] then c20() end
+  if b[3] then c21() end
+  if b[4] then c22() end
+  if b[5] then c23() end
+  if b[6] then c24() end
+  if b[7] then c25() end
+end
+
+function c19()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("710001101", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710003205", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001102", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710003206", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001103", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710003204", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("𝗧𝗘𝗠𝗣𝗟𝗔𝗥 𝗠𝗜𝗧𝗛𝗜𝗖 𝗔𝗖𝗧𝗜𝗩𝗔𝗧𝗘")
+end
+
+function c20()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("710001101", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710002568", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001102", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710002569", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001103", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710002570", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("𝗦𝗣𝗘𝗖𝗧𝗥𝗘 𝗔𝗖𝗧𝗜𝗩𝗔𝗧𝗘")
+end
+
+function c21()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("710001101", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710003067", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001102", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710003068", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001103", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710003069", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("𝗡𝗬𝗫 𝗟𝗘𝗚𝗘𝗡𝗗 𝗖𝗛𝗔𝗥𝗔𝗖𝗧𝗘𝗥 𝗗𝗢𝗡𝗘")
+end
+
+function c22()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("710001101", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710004111", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001102", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710004112", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001103", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710004113", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("𝗗𝗔𝗠𝗘 𝗟𝗘𝗚𝗘𝗡𝗗 𝗖𝗛𝗔𝗥𝗔𝗖𝗧𝗘𝗥 𝗗𝗢𝗡𝗘")
+end
+
+function c23()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("710001101", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710004858", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001102", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710004859", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001103", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710004860", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.alert("Skin Activated ✅")
+  gg.toast("DARK FIONA ACTIVATED")
+end
+
+function c24()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("710001101", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710001602", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001102", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710001591", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001103", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710001591", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("REAPER ACTIVATED")
+  gg.alert("REAPER ACTIVATED")
+end
+
+function c25()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("710001101", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710001280", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001102", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710001281", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("710001103", gg.TYPE_DWORD)
+  gg.getResults(10000)
+  gg.editAll("710001282", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("HOLY FATHER DONE")
 end
 
 function exit()
